aba0c2ffee202c6543c8e76c629cbad3537bf3cd
Victor Ingman <viregistermail@gmail.com>
2019-08-10 18:11:25 +0200

    update ultimate guitar instance

--- popup.js
+++ popup.js
@@ -10,8 +10,10 @@ function updateRange (event) {
 
 // send to content script
 function sendToCS (message) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message)
+  chrome.tabs.query({}, function (tabs) {
+    tabs.forEach((tab) => {
+      chrome.tabs.sendMessage(tab.id, message)
+    })
   })
 }
 

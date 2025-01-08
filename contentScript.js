40b0e3c19c7cf43a9717d6ed11676e781f7b5525
Victor Ingman <viregistermail@gmail.com>
2025-01-08 23:16:11 +0100

    Fullscreen support, dark mode support, fix chord highlights

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,7 @@
 const wrapperSelector = 'main';
 const tabsSelector = 'code > pre';
+const tabsWithPaddingSelector = 'section.P8ReX'
+const mainContentSelector = 'article.o2tA_.JJ8_m'
 
 function waitForElm(selector) {
   return new Promise(resolve => {
@@ -53,16 +55,150 @@ function maximizeViewport () {
   }
 }
 
-Promise.all([
-  waitForElm(wrapperSelector),
-  waitForElm(tabsSelector)
-]).then(() => {
-  maximizeViewport()
+function createButton (title) {
+  const $button = document.createElement('button')
+  const buttonClasses = 'rPQkl mcpNL IxFbd gm3Af lTEpj qOnLe'.split(' ')
+  $button.classList.add(...buttonClasses)
   
+  const $title = document.createElement('span')
+  const titleClasses = 'KNVWh _sWeD'.split(' ')
+  $title.classList.add(...titleClasses)
+  $title.textContent = title
+
+  $button.appendChild($title)
+
+  return $button
+}
+
+function setupStyles () {
+  const $style = document.createElement('style')
+  $style.id = "modest-guitar-color-scheme"
+  $style.textContent = `
+  @media (prefers-color-scheme: dark) {
+    ${mainContentSelector} {
+      filter: invert();
+    }
+    .mg-fullscreen ${mainContentSelector} {
+      filter: none;
+    }
+    .mg-fullscreen ${tabsWithPaddingSelector} {
+      filter: invert();
+    }
+  }
+  `
+
+  $style.textContent += `
+    .mg-fullscreen ${tabsWithPaddingSelector} {
+      overflow-y: scroll;
+      overflow-x: hidden;
+      background-color: white;
+      height: 100%;
+      padding: 0.5rem 0.75rem;
+    }
+    .mg-fullscreen ${tabsSelector} {
+      background-color: transparent;
+    }
+  `
+
+  $style.textContent += `
+    p.mg {
+      margin: 0;
+    }
+    .mg-action-bar {
+      display: flex;
+      align-items: center;
+      border: 1px solid rgba(0,0,0,0.25);
+      margin-bottom: 0.5rem;
+      margin-left: -0.5rem;
+      padding-left: 0.5rem;
+      gap: 0.5rem;
+    }
+  `
+  
+  document.body.append($style)
+}
+
+function createFullscreenButton (title) {
+  const $button = createButton(title)
+  const $icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  $icon.setAttribute('width', 16)
+  $icon.setAttribute('height', 16)
+  $icon.setAttribute('viewBox', '0 0 16 16')
+  $icon.setAttribute('fill', 'none')
+  $icon.innerHTML = `
+  <g>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.59109 8.82321C7.88398 9.11611 7.88398 9.59098 7.59109 9.88388L4.16431 13.3107L6.93943 13.3106C7.35365 13.3106 7.68945 13.6465 7.68945 14.0607C7.68946 14.4749 7.35368 14.8107 6.93946 14.8107L2.35366 14.8106C1.93945 14.8106 1.60366 14.4749 1.60366 14.0606L1.60364 9.47486C1.60365 9.06065 1.93943 8.72486 2.35364 8.72486C2.76786 8.72486 3.10365 9.06065 3.10364 9.47486L3.10365 12.25L6.53043 8.82321C6.82332 8.53032 7.2982 8.53032 7.59109 8.82321Z" fill="black"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.82331 7.59101C8.53042 7.29812 8.53042 6.82324 8.82331 6.53035L12.2501 3.10357L9.47497 3.10357C9.06075 3.10357 8.72494 2.76777 8.72494 2.35355C8.72494 1.93933 9.06072 1.60355 9.47494 1.60355L14.0607 1.60358C14.475 1.60357 14.8107 1.93937 14.8107 2.35358L14.8108 6.93936C14.8108 7.35358 14.475 7.68936 14.0608 7.68936C13.6465 7.68936 13.3108 7.35358 13.3108 6.93936L13.3108 4.16423L9.88397 7.59101C9.59108 7.8839 9.1162 7.8839 8.82331 7.59101Z" fill="black"/>
+  </g>
+    `
+  $icon.classList.add(...'is4YP iWDbw sTohX YEJsU'.split(' '))
+  $button.prepend($icon)
+  return $button
+}
+
+function handleFullscreen () {
+  const tabsWithPaddingWrapper = document.querySelector(tabsWithPaddingSelector)
+  tabsWithPaddingWrapper.requestFullscreen()
+}
+
+function setupControls () {
+  const tabsWrapper = document.querySelector(tabsSelector)
+  
+  const actionWrapper = document.createElement('section')
+  actionWrapper.classList.add('mg-action-bar')
+
+  const $mgLogo = document.createElement('p')
+  $mgLogo.classList.add('mg')
+  $mgLogo.textContent = 'Modest Guitar'
+  actionWrapper.appendChild($mgLogo)
+
+  const fsButton = createFullscreenButton('Fullscreen')
+  actionWrapper.appendChild(fsButton)
+
+  // add on top of tabs
+  tabsWrapper.insertAdjacentElement("beforebegin", actionWrapper)
+
+  fsButton.addEventListener('click', handleFullscreen)
+
+  return () => {
+    fsButton.removeEventListener('click', handleFullscreen)
+  }
+}
+
+function fixChordHighlight () {
+  // force every chord highlight to render no matter screen size
+  const script = document.createElement('script');
+  script.textContent = `window.innerHeight = 10_000;`;
+  document.documentElement.appendChild(script);
+  script.remove();
+}
+
+function setupListeners () {
+  document.addEventListener('fullscreenchange', () => {
+    const isFullscreen = document.fullscreenElement != null
+    if (isFullscreen) {
+      document.body.classList.add('mg-fullscreen')
+    } else {
+      document.body.classList.remove('mg-fullscreen')
+    }
+  })
+}
+
+async function init () {
+  await waitForElm(wrapperSelector)
+  await waitForElm(tabsSelector)
+  
+  setupStyles()
+  fixChordHighlight()
+  maximizeViewport()
+  setupListeners()
+  setupControls()
   getColumns((columnCount) => {
     setColumns(columnCount)
   })
-})
+}
+
+init()
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'setColumns') {
